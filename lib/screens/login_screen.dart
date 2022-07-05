11b3366745c8e104c11846736cbdd93f63575fcf
sk8rboil <sk8rboil008@windowslive.com>
2022-07-05 16:05:26 +0700

    add login-UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,14 +1,17 @@
-// ignore_for_file: prefer_const_constructors, no_leading_underscores_for_local_identifiers
+// ignore_for_file: prefer_const_constructors, no_leading_underscores_for_local_identifiers, non_constant_identifier_names
 
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+
+final _formKey = GlobalKey<FormState>();
+TextEditingController email_controller = TextEditingController();
+TextEditingController password_controller = TextEditingController();
 
 class MyLoginScreen extends StatelessWidget {
   const MyLoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: Text('MY-BMI'),
@@ -19,67 +22,64 @@ class MyLoginScreen extends StatelessWidget {
           child: Column(
             // ignore: prefer_const_literals_to_create_immutables
             children: <Widget>[
-              Lottie.asset(
-                'assets/json/17169-smooth-healthy-animation.json',
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      TextFormField(),
-                      TextFormField(),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text('LOGIN'),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        // ignore: prefer_const_literals_to_create_immutables
-                        children: <Widget>[
-                          Divider(
-                            height: 10,
-                            color: Colors.black,
-                          ),
-                          Text('data'),
-                          Divider(
-                            height: 10,
-                            color: Colors.black,
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.08,
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                shape: CircleBorder(),
-                              ),
-                              onPressed: () {},
-                              child: Text('social'),
-                            ),
-                          ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.08,
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                shape: CircleBorder(),
-                              ),
-                              onPressed: () {},
-                              child: Text('social'),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              logoapp(context),
+              loginform(context),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Container logoapp(BuildContext context) {
+    return Container(
+      child: Lottie.asset(
+        height: MediaQuery.of(context).size.height * 0.4,
+        fit: BoxFit.fill,
+        'assets/json/17169-smooth-healthy-animation.json',
+      ),
+    );
+  }
+
+  Padding loginform(context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: email_controller,
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextFormField(
+              controller: password_controller,
+              obscureText: true,
+              keyboardType: TextInputType.visiblePassword,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('LOGIN'),
+              ),
+            ),
+            Divider(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.amber,
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/register_screen');
+                },
+                child: Text('REGISTER'),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -9,11 +9,43 @@ class MyRegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        // ignore: prefer_const_literals_to_create_immutables
-        children: <Widget>[
-          Text('REGISTER SCREEN'),
-        ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: <Widget>[
+              Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  VerticalDivider(
+                    width: 10,
+                  ),
+                  Text(
+                    'REGISTER PAGE',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Form(
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(),
+                    TextFormField(),
+                    TextFormField(),
+                    TextFormField(),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('REGISTER'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
